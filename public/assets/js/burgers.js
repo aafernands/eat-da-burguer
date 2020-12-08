8174e4fdae69fd3d0e64237bb743071b54ae9e28
Alex Fernandes <alexfernands@outlook.com>
2020-12-07 19:30:13 -0500

    add script to post data to server

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,1 +1,19 @@
 console.log("burger file");
+$(function () {
+	$("#burger-form").on("submit", function (event) {
+		event.preventDefault();
+		console.log("submit");
+		const burgerName = $(this).children("input").val();
+
+		$.ajax({
+			method: "POST",
+			url: "/burgers",
+			data: {
+				name: burgerName,
+			},
+		}).then(function (response) {
+			console.log("******************** response **********************");
+			console.log(response);
+		});
+	});
+});
