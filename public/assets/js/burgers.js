1ec94b174bfbcf34290642a9c25505b949534843
Alex Fernandes <alexfernands@outlook.com>
2020-12-14 20:57:32 -0500

    updated style

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,65 +1,67 @@
 $(function () {
 	const $notDevouredList = $("#not_devoured");
 	const $devouredList = $("#devoured");
-	$("#burger-form").on("submit", function (event) {
-		event.preventDefault();
-		console.log("submit");
-		const burgerName = $(this).children("input").val();
 
-		$.ajax({
-			method: "POST",
-			url: "/burgers",
-			data: {
-				name: burgerName,
-			},
-		}).then(function (response) {
-			location.reload();
-		});
-	});
+	function attachEvent() {
+		$("#burger-form").on("submit", function (event) {
+			event.preventDefault();
+			console.log("submit");
+			const burgerName = $(this).children("input").val();
 
-	$("#not_devoured").on("click", "button", function (event) {
-		const id = $(this).data("id");
-		console.log(id);
-		$.ajax({
-			method: "PUT",
-			url: "/burgers/" + id,
-		}).then(function (response) {
-			location.reload();
+			$.ajax({
+				method: "POST",
+				url: "/burgers",
+				data: {
+					name: burgerName,
+				},
+			}).then(function (response) {
+				location.reload();
+			});
 		});
-	});
 
-	$("#devoured").on("click", "button", function (event) {
-		const id = $(this).data("id");
-		console.log(id);
-		$.ajax({
-			method: "DELETE",
-			url: "/burgers/" + id,
-		}).then(function (response) {
-			location.reload();
+		$("#not_devoured").on("click", "button", function (event) {
+			const id = $(this).data("id");
+			console.log(id);
+			$.ajax({
+				method: "PUT",
+				url: "/burgers/" + id,
+			}).then(function (response) {
+				location.reload();
+			});
 		});
-	});
+
+		$("#devoured").on("click", "button", function (event) {
+			const id = $(this).data("id");
+			console.log(id);
+			$.ajax({
+				method: "DELETE",
+				url: "/burgers/" + id,
+			}).then(function (response) {
+				location.reload();
+			});
+		});
+	}
 
 	function getAllBurger() {
 		$.ajax({
 			method: "GET",
 			url: "/burgers",
 		}).then(function (response) {
-			console.log(response);
-
+			console.log("all burger from server: ", response);
 			renderNotDevoured(response.filter((burger) => burger.devoured === 0));
 			renderDevoured(response.filter((burger) => burger.devoured === 1));
 		});
 	}
 
 	function renderNotDevoured(burgers) {
-		console.log("not de", burgers);
+		console.log("not devoured burger", burgers);
 		let html = "";
 		burgers.forEach((burger) => {
 			html += `
 			<div class="row burger-row">
-                <div class="col-md-9 text-center">${burger.id} ${burger.burger_name}</div>
-                <div class="col-md-3">
-                    <button class="btn btn-primary devour" type="button" data-id="${burger.id}">Devour</button>
+                <div class="col-md-6 col-sm-6 ">${burger.id} ${burger.burger_name}</div>
+                <div class="col-md-6 col-sm-6 ">
+                    <button class="btn-sm btn-primary devour" type="button" data-id="${burger.id}">Devour</button>
                 </div>
 			</div>
 			`;
@@ -69,14 +71,14 @@ $(function () {
 	}
 
 	function renderDevoured(burgers) {
-		console.log("de", burgers);
+		console.log("devoured burger", burgers);
 		let html = "";
 		burgers.forEach((burger) => {
 			html += `
 			<div class="row burger-row">
-                <div class="col-md-9 text-center">${burger.id} ${burger.burger_name}</div>
-                <div class="col-md-3">
-                    <button class="btn btn-danger devour" type="button" data-id="${burger.id}">Delete</button>
+                <div class="col-md-6">${burger.id} ${burger.burger_name}</div>
+                <div class="col-md-6">
+                    <button class="btn-sm btn-danger delete" type="button" data-id="${burger.id}">Delete</button>
                 </div>
 			</div>
 			`;
@@ -86,6 +88,7 @@ $(function () {
 	}
 
 	function start() {
+		attachEvent();
 		getAllBurger();
 	}
 
